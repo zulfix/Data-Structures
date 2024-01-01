ec87c8289b64e392da335d4f51a064ec0d4b0836
zulfix <122185036+zulfix@users.noreply.github.com>
2024-01-01 16:32:28 -0500

    Update 2.30 LAB- Convert to dollars.cpp

--- 2.30 LAB- Convert to dollars.cpp
+++ 2.30 LAB- Convert to dollars.cpp
@@ -1,18 +1,3 @@
-/*2.30 LAB: Convert to dollars
-
-Given three input values representing counts of nickels, dimes, and quarters, output the total amount as dollars and cents.
-
-Output each floating-point value with two digits after the decimal point using the following statement once before all other cout statements:
-cout << fixed << setprecision(2);
-
-Ex: If the input is:
-
-3 1 4
-where 3 is the number of nickels (at $0.05 each), 1 is the number of dimes (at $0.10 each), and 4 is the number of quarters (at $0.25 each), the output is:
-
-Amount: $1.25
-For simplicity, assume input is non-negative.*/
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -33,3 +18,18 @@ int main() {
    
    return 0;
 }
+
+/*2.30 LAB: Convert to dollars
+
+Given three input values representing counts of nickels, dimes, and quarters, output the total amount as dollars and cents.
+
+Output each floating-point value with two digits after the decimal point using the following statement once before all other cout statements:
+cout << fixed << setprecision(2);
+
+Ex: If the input is:
+
+3 1 4
+where 3 is the number of nickels (at $0.05 each), 1 is the number of dimes (at $0.10 each), and 4 is the number of quarters (at $0.25 each), the output is:
+
+Amount: $1.25
+For simplicity, assume input is non-negative.*/
