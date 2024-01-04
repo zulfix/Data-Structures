caafe131fedd13981483c4b9e38715553f124850
Zulfi <122185036+zulfix@users.noreply.github.com>
2024-01-04 15:25:15 -0500

    Update 4.15 LAB- Display a character.cpp

--- 4.15 LAB- Display a character.cpp
+++ 4.15 LAB- Display a character.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+char inputChar;
+
+while (true) {
+   cout << "Enter a character: ";
+   cin >> inputChar;
+   
+   if (inputChar == 'Z') {
+      cout << "Bye!" << endl;
+      break;
+   } else {
+   cout << inputChar << endl;
+   }
+
+}
+   return 0;
+}
+
 /*4.15 LAB: Display a character
 Create a while loop to display a character that is entered by a user each time until he/she types in a Z, then print out Bye!.
 
